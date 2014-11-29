1c7b8712daf8c859b5ba8c7b030f02497b22b3fc
Jake Martin <jmart.alb@gmail.com>
2014-11-28 22:50:23 -0500

    YASSSS ALMOST DOOOOOOONE

---
+++ compiledistributed.bat
@@ -0,0 +1,3 @@
+java -cp salsa1.1.5.jar;. salsac.SalsaCompiler pa3/*.salsa
+javac -classpath salsa1.1.5.jar;. pa3/*.java
+java -cp salsa1.1.5.jar;. pa3.IndexWithTheaters collection index 10 30 theaters.txt
